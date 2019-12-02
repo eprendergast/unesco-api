60e7efb97f7e586755616054798865f78e597b32
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-12-02 18:38:30 +0000

    uncommented cashing

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,14 +1,8 @@
 class SitesController < ApplicationController
 
     def index
-        @cached_sites = @cached_sites || nil
-
-        if @cached_sites == nil
-            sites = Site.all.select{ |site| site.image_url != "https://images.unsplash.com/photo-1548686013-c85877abc345?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" }
-            @cached_sites = SiteSerializer.new(sites).to_serialized_json
-        end
-        
-        render json: @cached_sites
+        sites = Site.all.select{ |site| site.image_url != "https://images.unsplash.com/photo-1548686013-c85877abc345?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" }
+        render json: SiteSerializer.new(sites).to_serialized_json
     end
 
     def show
